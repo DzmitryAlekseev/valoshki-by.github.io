f970248721a21fdacad7aeb670caf28cce2b429e
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2022-06-17 21:00:52 +0300

    fgh

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// function switchAnswer(ele) {
-//     //console.log(ele);
-//     var faqAnswer = ele.querySelector(".info__element p");
-//     faqAnswer.classList.toggle("faq__answer_hidden");
-//   }
-
-console.log(querySelector(".info__element"));
