f42c640d0f0245f0713c338a6ff35d3e5b9ec111
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2022-06-17 19:37:19 +0300

    ed

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// function switchAnswer(ele) {
+//     //console.log(ele);
+//     var faqAnswer = ele.querySelector(".info__element p");
+//     faqAnswer.classList.toggle("faq__answer_hidden");
+//   }
+
+console.log(querySelector(".info__element"));
