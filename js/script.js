e03b2c6a0c6cf656cb6a0f40fc57f82c5ceeef25
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2022-06-22 10:30:02 +0300

    c

--- js/script.js
+++ js/script.js
@@ -31,3 +31,46 @@ function switchAnswer(ele) {
       var faqAnswer = ele.querySelector(".info-element_p");
       faqAnswer.classList.toggle("info-element_p_hiden");
     }
+
+    //menu
+    jQuery(function ($) {
+        if ($) {
+          console.log("jQuery start");
+        }
+        
+        
+        var navMenu = $(".menu"); // выбираем меню
+        console.log(navMenu.offset().top)
+        
+        var navMenuTop = navMenu.offset().top; //получение знaчения св-ва top
+        var toTop = $(".to-top"); // выбираем кнопку to-top
+        var body = $("html, body");
+    
+        $(window).on("resize", function () {
+          console.log("окно изменило размер");
+          // получаем св-во top каждый раз при изменения размеров окна браузера
+          navMenuTop = navMenu.offset().top;
+          console.log("navMenuTop: ", navMenuTop);
+    
+          console.log("height:", $(window).innerHeight()); // высота viewport
+          console.log("width:", $(window).innerWidth());   // ширина viewport
+        });
+    
+        $(window).on("scroll", function () {
+          var winScrollTopValue = $(window).scrollTop(); // насколько проскроллили страницу
+    
+          console.log(winScrollTopValue);
+    
+          if (winScrollTopValue > navMenuTop) {
+            console.log("Ятут")
+            navMenu.addClass("menu_fixed");
+          } else {
+            console.log("Ятут а")
+            navMenu.removeClass("menu_fixed");
+          }
+    
+      
+        });
+
+   
+    })
